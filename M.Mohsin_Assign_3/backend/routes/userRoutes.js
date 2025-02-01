a0eeef4036801d221170597614b519d5b72099fd
mohsinakabilsins <mohsinakabilsins@gmail.com>
2025-02-02 03:31:50 +0500

    complete change

--- M.Mohsin_Assign_3/backend/routes/userRoutes.js
+++ M.Mohsin_Assign_3/backend/routes/userRoutes.js
@@ -1,13 +1,25 @@
+// import express from 'express';
+// import userController from '../controllers/UserLogic.js';
+
+
+// const router = express.Router();
+
+// router.post("/",userController.createUser);
+
+// router.post('/signin', userController.loginUser);
+
+// router.post('/signout', userController.logoutUser);
+
+// export default router;
+// routes/userRoutes.js
+
 import express from 'express';
 import userController from '../controllers/UserLogic.js';
 
-
 const router = express.Router();
 
-router.post("/",userController.createUser);
-
-router.post('/signin', userController.loginUser);
-
-router.post('/signout', userController.logoutUser);
+router.post("/", userController.createUser); // Signup route
+router.post('/signin', userController.loginUser); // Login route
+router.post('/signout', userController.logoutUser); // Logout route
 
 export default router;
